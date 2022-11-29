78f22e381907490ad76b95bc39406ad4f2e24a7e
duuree17 <duuren0315@gmail.com>
2022-11-29 15:05:30 +0800

    11/29

--- deeznuts/src/Components/Header.js
+++ deeznuts/src/Components/Header.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 export const Header = () => {
   const updatePlaylist = async () => {
     const res = await axios.put(
-      `https://6375fb74b5f0e1eb85fed196.mockapi.io/api/v1/users/17/playlists/1`,
-      {songs:{ artist: 'Drake', id: '6969', link: 'songLink', name: 'Hotline Bling' }},
+      `https://6375fb74b5f0e1eb85fed196.mockapi.io/api/v1/users/17/playlists`,
+      {songs:{ artist: 'Drake', id: '6969', link: 'songLink', name: 'Hotline Bling',cardon:'temka' }},
     )
     console.log(res)
   }
